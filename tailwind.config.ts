8ebaee5d68492e5d868f9fab98b9f0f1c221f396
josserden <josserden@gmail.com>
2023-12-13 10:36:33 +0200

    fix: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  mode: 'jit',
   content: [
     './app/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
