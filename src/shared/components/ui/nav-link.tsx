f758db7345f423daaec5013d1bad7b652926b754
josserden <josserden@gmail.com>
2026-03-16 22:36:32 +0200

    refactor: update NavLink component to handle href prop more robustly

--- src/shared/components/ui/nav-link.tsx
+++ src/shared/components/ui/nav-link.tsx
@@ -9,10 +9,11 @@ import type { ComponentProps } from 'react';
 
 export function NavLink({ className, ...props }: ComponentProps<typeof Link>) {
   const pathname = usePathname();
+  const href = typeof props.href === 'string' ? props.href : props.href.pathname;
 
   return (
     <Link
-      data-active={pathname === props.href || undefined}
+      data-active={pathname === href || undefined}
       className={cn(
         'hover:underline data-active:pointer-events-none data-active:font-bold data-active:underline',
         className,
