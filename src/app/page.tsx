cca79deca6b7a2c68055cf4ea037366e013720fe
josserden <josserden@gmail.com>
2026-03-16 19:47:22 +0200

    feat: add Typography component and update page layout with new imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,24 @@
-import { Form } from '@/shared/components/forms/form';
+import { Form } from '@/features/example/components/form';
+import { Typography } from '@/shared/components/ui/typography';
 
 export default function Home() {
   return (
     <section className='py-20'>
       <div className='container space-y-10'>
         <div className='space-y-10 text-center'>
-          <h1 className='text-3xl font-bold text-gray-700'>Next.js Boilerplate</h1>
-          <p className='text-base text-gray-800'>
-            Modern starter built with Next.js 16, React 19, TypeScript, Tailwind CSS 4,
+          <Typography className='text-gray-700' variant='h1' as='h1'>
+            Next.js Boilerplate
+          </Typography>
+
+          <Typography className='text-gray-800'>
+            Modern starter built with Next.js, React 19, TypeScript, Tailwind CSS 4,
             <br />
-            TanStack Query, React Hook Form, Radix UI, and more.
-          </p>
-          <h2 className='text-2xl font-bold'>Example Form</h2>
+            next-safe-action, TanStack Query, React Hook Form, and Radix UI.
+          </Typography>
+
+          <Typography variant='h2' as='h2'>
+            Example Action Form
+          </Typography>
         </div>
 
         <Form />
