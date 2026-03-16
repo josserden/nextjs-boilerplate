0cfd600112c1cb98f0240855778a0700b57682ef
josserden <josserden@gmail.com>
2026-03-16 22:41:40 +0200

    refactor: restructure env.ts to define shared environment variables

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -1,10 +1,13 @@
+import { Typography } from '@/shared/components/ui/typography';
 import { siteConfig } from '@/shared/config/site';
 
 export function Footer() {
   return (
     <footer className='border-t border-slate-200 py-2'>
       <div className='container text-center'>
-        {siteConfig.author.name} &copy; {new Date().getFullYear()}
+        <Typography>
+          {siteConfig.author.name} &copy; {new Date().getFullYear()}
+        </Typography>
       </div>
     </footer>
   );
