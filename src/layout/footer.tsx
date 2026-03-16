f6f7fed993b22cf82cea30cd4d6f1d571e9d926b
josserden <josserden@gmail.com>
2026-03-16 22:26:06 +0200

    refactor: streamline footer component by consolidating author and year display

--- src/layout/footer.tsx
+++ src/layout/footer.tsx
@@ -4,9 +4,7 @@ export function Footer() {
   return (
     <footer className='border-t border-slate-200 py-2'>
       <div className='container text-center'>
-        {siteConfig.author.name}
-        &copy;
-        {new Date().getFullYear()}
+        {siteConfig.author.name} &copy; {new Date().getFullYear()}
       </div>
     </footer>
   );
